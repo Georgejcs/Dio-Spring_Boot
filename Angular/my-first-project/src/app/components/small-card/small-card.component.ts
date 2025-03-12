6b804ec91419695d764596aaa562abcd1499cc64
George José <georgejos756@gmail.com>
2025-03-12 20:38:12 -0300

    Criando um Blog com Angular

---
+++ Angular/my-first-project/src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  imports: [],
+  templateUrl: './small-card.component.html',
+  styleUrl: './small-card.component.css'
+})
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTtile:string = ""
+  @Input()
+  id:string = "0"
+
+  constructor() {}
+  ngOnInit(): void {
+  }
+
+}
---
+++ Angular/my-first-project/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+   {
+    "id": "1",
+    "title": "Gachiakuta",
+    "description": "Anime de gachiakuta é anunciado",
+    "photoCover": "https://conectageek.com.br/wp-content/uploads/2024/06/gachiakuta_anime.jpeg"
+   },
+   {
+    "id": "2",
+    "title": "Doctor Doom em ‘AVENGERS: DOOMSDAY’",
+    "description": "Doctor Doom",
+    "photoCover": "https://maxblizz.com/wp-content/uploads/2025/03/Robert-Downey-Jr-as-Doctor-Doom.jpg"
+   }
+]
